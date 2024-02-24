03dac2db71d152d5d6400f9616deb8731d7be9cf
Akash <akashramnagr282@gmail.com>
2024-02-25 02:31:37 +0530

    update commingsoon,favicon,vercel

---
+++ src/app/home/page.tsx
@@ -0,0 +1,11 @@
+
+"use client"
+import React from "react";
+import { Commingsoon } from "@/components/Commingsoon";
+export default function home(){
+    return(
+    <>
+     <Commingsoon/>
+    </>
+    )
+}
---
+++ src/components/Commingsoon.tsx
@@ -0,0 +1,25 @@
+import "./Commingsoon.css";
+export const Commingsoon=()=> {
+  return <>
+  <div className="contain">
+   <div className="wrapper">
+		<span></span>
+		<span></span>
+		<span></span>
+		<span></span>
+		<span></span>
+		<span></span>
+		<span></span>
+		<span></span>
+		<span></span>
+		<span></span>
+</div>
+
+<div className="banner">
+	<div className="content">
+		<h2><b>Coming Soon ...</b></h2>
+	</div>
+</div>
+</div>
+  </> 
+}
