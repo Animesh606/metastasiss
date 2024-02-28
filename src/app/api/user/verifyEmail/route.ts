87c3da00f23aea3c864816f53d16501013021be7
Animesh <2025.cse18.animeshbag@gmail.com>
2024-02-29 01:50:52 +0530

    Added: verifyEmail to verify user with token and send user registration success mail

---
+++ src/app/api/user/verifyEmail/route.ts
@@ -0,0 +1,43 @@
+import User from "@/models/user.model";
+import sendEmail from "@/utils/mailer";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest) {
+    try {
+        // Get token from request
+        const { token } = await req.json();
+
+        // Find user with the token
+        const user = await User.findOne({
+            verificationToken: { $exists: true, $eq: token },
+        });
+        
+        // In case no user found in database
+        if (!user) {
+            return NextResponse.json(
+                { message: "Invalid Token" },
+                { status: 400 }
+            );
+        }
+
+        // Verify the user
+        await user.verifyUser();
+
+        // Send Registration Successful mail
+        await sendEmail("userRegistration", {
+            email: user.email,
+            fullName: user.fullName
+        })
+
+        return NextResponse.json(
+            { message: "Email Verified Successfully" },
+            { status: 200 }
+        );
+    } catch (error) {
+        console.log("Error on /api/user/verifyEmail", error);
+        return NextResponse.json(
+            { message: "Internal Server Error" },
+            { status: 500 }
+        );
+    }
+}
