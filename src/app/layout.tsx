a49729bc965777621f6499e65ffc003d9ab5b921
Animesh <2025.cse18.animeshbag@gmail.com>
2024-03-06 16:22:24 +0530

    Updated: layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,33 +1,39 @@
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Navbar } from "@/components/Navbar";
-import { config } from '@fortawesome/fontawesome-svg-core'
-import '@fortawesome/fontawesome-svg-core/styles.css'
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
 import { Toaster } from "react-hot-toast";
-config.autoAddCss = false
+import SessionProvider from "./Provider";
+import { getServerSession } from "next-auth";
+config.autoAddCss = false;
 const inter = Inter({ subsets: ["latin"] });
-import { AOSInit } from './aos'
+import { AOSInit } from "./aos";
 
 export const metadata: Metadata = {
-  title: "Meta-stasiss",
-  description: "AIIMS GUWAHATI",
+    title: "Meta-stasiss",
+    description: "AIIMS GUWAHATI",
 };
-export default function RootLayout({
-  children,
+export default async function RootLayout({
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-       <AOSInit />
-      <body className={inter.className}>
-        <Navbar/>
-        <Toaster position="bottom-center" />
-        {children}
-        <SpeedInsights/>
-      </body>
-    </html>
-  );
+    const session = await getServerSession();
+    return (
+        <html lang="en">
+            <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+            <SessionProvider session={session}>
+                <AOSInit />
+                <body className={inter.className}>
+                    <Navbar />
+                    <Toaster position="bottom-center" />
+                    {children}
+                    <SpeedInsights />
+                </body>
+            </SessionProvider>
+        </html>
+    );
 }
