c058ba2ffbdb59018f25d27386253e88bd2c05e7
Akash <akashramnagr282@gmail.com>
2024-03-23 19:32:34 +0530

    update:id card update section in cloudinary

--- src/utils/uploadImage.ts
+++ src/utils/uploadImage.ts
@@ -2,17 +2,7 @@ import cloudinary from "@/../cloudinary.config";
 import { unlink, writeFile } from "fs/promises";
 
 export async function uploadImage(image: File) {
-    // Make buffer and store file to local storage
-    const bufferData = await image.arrayBuffer();
-    const buffer = Buffer.from(bufferData);
     const path = `./src/app/api/upload/${image.name}`;
-    await writeFile(path, buffer);
-
-    // Upload the image to Cloudinary
     const result = await cloudinary.uploader.upload(path, { public_id: "WebData/Metastasiss/CollegeId/" + image.name + Date.now() });
-
-    // Delete the localfile
-    await unlink(path);
-
     return result.secure_url;
 }
